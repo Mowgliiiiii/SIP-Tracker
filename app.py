50f851b56cb172827865c6b8484c0eaccf242dad
Mowgliiiiii <atul821111@gmail.com>
2026-06-19 01:56:50 +0530

    testing buttons

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 class MyApp(App):
+    def on_button_click(self,instance):
+        print('Button Clicked')
+    
     def build(self):
-        return Label(text="Hello World")
+        layout = BoxLayout(orientation = 'vertical')
+        layout.add_widget(Label(text="Hello World",size_hint=(1,0.3)))
+        layout.add_widget(Button(text="Get Started",size_hint=(1,0.7),on_press=self.on_button_click))
+        return layout
     
 MyApp().run()
