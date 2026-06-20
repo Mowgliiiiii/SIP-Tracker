79c3a2141167d19a99151ffafe60b76a81151dcb
Mowgliiiiii <atul821111@gmail.com>
2026-06-20 20:49:39 +0530

    created UI - added user input and a button

--- app.py
+++ app.py
@@ -4,14 +4,58 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
+from datetime import datetime
+
 class MyApp(App):
     def on_button_click(self,instance):
-        print('Button Clicked')
+        check_str = ""
+
+        try:
+            scheme_code = int(self.scheme_input.text)
+        except:
+            check_str += "Please enter the valid scheme code\n"
+        
+        try:
+            date_input = datetime.strptime(self.date_input.text,"%d-%m-%y")
+        except:
+            check_str  += "Please enter the date in the format: dd-mm-yy\n"
+
+        try:
+            amount = float(self.amount_input.text)
+        except:
+            check_str += "Please enter the valid amount"
+        
+        if check_str == "":
+            self.label.text = "Investment added successfully"
+        else:
+            self.label.text = check_str
     
     def build(self):
-        layout = BoxLayout(orientation = 'vertical')
-        layout.add_widget(Label(text="Hello World",size_hint=(1,0.3)))
-        layout.add_widget(Button(text="Get Started",size_hint=(1,0.7),on_press=self.on_button_click))
-        return layout
+        outer_layout = BoxLayout(orientation = 'vertical')
+        
+        inner_layout1 = BoxLayout(orientation = 'horizontal',size_hint=(1,0.2))
+        inner_layout2 = BoxLayout(orientation = 'horizontal',size_hint=(1,0.2))
+        inner_layout3 = BoxLayout(orientation = 'horizontal',size_hint=(1,0.2))
+
+        self.label = Label(text="Get Started",size_hint=(1,0.2))
+        outer_layout.add_widget(self.label)
+
+        outer_layout.add_widget(inner_layout1)
+        self.scheme_input = TextInput(size_hint=(0.7,1))
+        inner_layout1.add_widget(Label(text="Enter Scheme Code:",size_hint=(0.3,1)))
+        inner_layout1.add_widget(self.scheme_input)
+
+        outer_layout.add_widget(inner_layout2)
+        self.date_input = TextInput(size_hint=(0.7,1),hint_text=("Format: dd-mm-yy"))
+        inner_layout2.add_widget(Label(text="Enter Date: ",size_hint=(0.3,1)))
+        inner_layout2.add_widget(self.date_input)
+
+        outer_layout.add_widget(inner_layout3)
+        self.amount_input = TextInput(size_hint=(0.7,1))
+        inner_layout3.add_widget(Label(text="Enter the amount you have invested:",size_hint=(0.3,1)))
+        inner_layout3.add_widget(self.amount_input)
+
+        outer_layout.add_widget(Button(text="Click to submit",size_hint=(1,0.2),on_press=self.on_button_click))
+        return outer_layout
     
 MyApp().run()
