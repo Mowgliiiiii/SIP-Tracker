255c5173146ce5c61588174312f9390c0d44aa4b
Mowgliiiiii <atul821111@gmail.com>
2026-06-23 01:32:53 +0530

    Wrapping in the function

--- add_data.py
+++ add_data.py
@@ -1,45 +1,52 @@
 import requests
 from datetime import datetime
-from database import add_fund,add_instalments, create_tables
+from database import add_fund,add_instalments, get_all_funds
 
-print("1. Add new fund")
-print("2. Add instalment to existing fund")
-
-choice = int(input())
-
-if choice==1:
-    fund_name = input("Fund name: ")
-    scheme_code = input("Scheme_code: ")
-
+def add_new_fund(scheme_code,fund_name):
     add_fund(scheme_code,fund_name)
 
-else:
-    scheme_code = (input('Enter Scheme_code: '))
-    input_date = input('Enter date in dd-mm-yy format: ')
-    amount = int(input('Enter the amount you have invested: '))
+def add_new_instalments(scheme_code,input_date,amount):
+    try:
+        scheme_code = scheme_code
+    except:
+        message = "Wrong scheme code"
+        return message
     
     url = f"https://api.mfapi.in/mf/{scheme_code}"
     response = requests.get(url)
     data = response.json()
-
+    
+    fund_present = False
+    for rows in get_all_funds:
+        if scheme_code == rows[0]:
+            fund_present = True
+        
+    if not fund_present:
+        add_new_fund(scheme_code,data['meta']['scheme_name'])
+        
     date = input_date[:-2] + "20" + input_date[-2:]
 
     check_date = datetime.strptime(date,"%d-%m-%Y")
     date_loop = 0
+    nav_availble = True
+    
     while date!=data['data'][date_loop]['date']:
         if check_date > datetime.strptime(data['data'][date_loop]['date'],"%d-%m-%Y"):
             if date_loop == 0:
-                print('NAV is not available yet')
+                nav_availble = False
             else:
                 date_loop-=1
             break
         date_loop+=1
-    
-    if(date_loop!=0):
+
+    if nav_availble:
         nav_at_purchase = float(data['data'][date_loop]['nav'])
-        print(nav_at_purchase)
 
         units = amount/nav_at_purchase
-        print(units)
 
-    #add_instalments(scheme_code,date,amount,units,nav_at_purchase)
+        add_instalments(scheme_code,date,amount,units,nav_at_purchase)
+        message = "Successfully added"
+        return message
+    else:
+        message = "Date not found"
+        return message
